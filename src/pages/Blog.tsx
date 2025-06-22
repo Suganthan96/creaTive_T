e6cb7be3e6aee1be55160ae26bf24839f89d2020
Suganthan96 <suganthan.27it@licet.ac.in>
2025-06-22 13:35:14 +0530

    title_change_T

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -16,7 +16,7 @@ const Blog = () => {
   return (
     <PageLayout>
       <SEO 
-        title="WRLDS - News and insights about smart textile technology" 
+        title="Creative Suganth" 
         description="Stay updated with the latest news and insights about sensor-integrated textiles and smart technology from WRLDS Technologies."
         imageUrl={featuredPost?.imageUrl || "/lovable-uploads/6b0637e9-4a7b-40d0-b219-c8b7f879f93e.png"}
         keywords={['smart textiles', 'textile technology', 'industry news', 'sensor innovation', 'wearable tech', 'smart fabrics']}
